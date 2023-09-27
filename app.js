32b749352c708d0c614f6f21c7d8acb413292ad7
CodeCassie4169E1 <clipcassie@yahoo.com>
2023-09-27 15:00:12 -0500

    Javascript update
    wrote basic javascript for current date and time of the two locations and have them updating every second

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+setInterval(function () {
+  let targetCityElement = document.querySelector("#target-city");
+  let targetDateElement = targetCityElement.querySelector(".date");
+  let targetTimeElement = targetCityElement.querySelector(".time");
+  let targetTime = moment().tz("America/New_York");
 
+  targetDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  targetTimeElement.innerHTML = `${targetTime.format(
+    "h:mm:ss"
+  )} <small>${targetTime.format("A")}</small>`;
+
+  let firstCityElement = document.querySelector("#first-city");
+  let firstDateElement = firstCityElement.querySelector(".date");
+  let firstTimeElement = firstCityElement.querySelector(".time");
+  let firstTime = moment().tz("Europe/London");
+
+  firstDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  firstTimeElement.innerHTML = `${firstTime.format(
+    "h:mm:ss"
+  )} <small>${firstTime.format("A")}</small>`;
+}, 1000);
